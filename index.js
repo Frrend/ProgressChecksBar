08458a26c22c2811539923b81c7ef9bace3207a5
Frrend <edualfd0514@gmail.com>
2024-09-14 15:06:19 +0200

    Imported all the elements in js and created made the progress bar increase when checking the circles.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Happy developing ✨')
+let progression = document.querySelector('.progression');
+
+let progressLength = 0;
+
+let wakeUp = document.querySelector('.wakeUp');
+let getUp = document.querySelector('.getUp');
+let shit = document.querySelector('.shit');
+let breakfest = document.querySelector('.breakfast');
+let teeth = document.querySelector('.teeth');
+
+const list = [wakeUp, getUp, shit, breakfest, teeth];
+
+for (let i = 0  ; i < list.length; i++) {
+    list[i].addEventListener('click', () => {
+        list[i].classList.toggle('onClass');
+        if (list[i].classList.contains('onClass')) {
+            progressLength = i + 1;
+        }
+        progress(progressLength)
+
+        if (progressLength === i + 1) {
+            progressLength = i;
+        }
+    })
+}
+
+function progress(steps) {
+    progression.style.width = `calc(100%/5*${steps})`;
+    progressLength = steps;
+}
